bff959cc06ff481b576932df53b023cf1922826e
rozer4722 <rozerbagh@gmail.com>
2024-12-13 16:12:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
 import express from "express"
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import todoRouter from "./routes/todo.routes.js";
-import usersRouter from "./routes/users.js";
-
-//  ====== CommonJS FORM OF Expres Imports ====== //
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
 
 const app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use('/todo', todoRouter);
-app.use('/users', usersRouter);
 
 app.get("/getmyapi", (req,res)=>{
   req.send({
@@ -28,23 +12,6 @@ app.get("/getmyapi", (req,res)=>{
   }
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 app.listen(4033, ()=>{
   console.log("Server is running on the port 4033");
 })
-export default app
